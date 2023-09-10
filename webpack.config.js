f42712cde7ef1ff5b1845cdf44e7a2c3673d92c2
Zaslavskiy Sergey <Zaslavskiy.sergeyxz@yandex.ru>
2023-09-10 20:29:39 +0300

    Настроил сборку

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,23 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
-const HtmlPlugin = require('html-webpack-plugin');
+
 module.exports = {
   // Точка входа
   entry: './src/main.js',
   output: {
     // Имя бандла
     filename: 'bundle.js',
-     // Директория для файлов сборки
+    // Директория для файлов сборки
     path: path.resolve(__dirname, 'build'),
     // Удаляем предыдущую сборку перед созданием новой
     clean: true,
   },
-// Генерируем карту исходного кода
-devtool: 'source-map'
-// Подключаем плагины
-plugins: [
-  new CopyPlugin({
-    patterns: [{ from: 'public' }],
-  }),
-],
+  // Генерируем карту исходного кода
+  devtool: 'source-map',
+  // Подключаем плагины
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: 'public' }],
+    }),
+  ],
+};
